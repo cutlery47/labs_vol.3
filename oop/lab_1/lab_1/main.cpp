8ef5fd70f124390926e53fc1e0e19d3b52db6ec9
Arkhip McNutt <ortiz47@mail.ru>
2023-09-03 13:00:39 +0300

    first lab

---
+++ oop/lab_1/lab_1/main.cpp
@@ -0,0 +1,10 @@
+#include <matrix.h>
+#include <application.h>
+
+
+//программма написана в кодировке windows-1251
+int main()
+{
+    //запускаем приложение через конструктор класса Application
+    Application app;
+}
